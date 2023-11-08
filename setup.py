53bd8ed0eb0b1af80b9dc9f1d22bc9df049b46aa
SpiffyB <50279056+SpiffyB@users.noreply.github.com>
2023-11-08 22:06:25 +0000

    added setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='r2p',
+    version='0.1.0',
+    description='Convert Rekordbox XML to .m3u8 playlist.',
+    long_description=readme,
+    author='Spiff',
+    url='https://github.com/SpiffyB/r2p',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
